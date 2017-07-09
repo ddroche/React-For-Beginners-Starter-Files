18987329e3df24bf8d150826af0850075387fa3e
Derek Roche <droche@nerdery.com>
2017-07-09 00:16:43 -0500

    Completed tutorial

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyALh0m37qWoxSVUzmEPtR0oVRFqLB6cGIU",
+    authDomain: "catch-of-the-day-derek-roche.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-derek-roche.firebaseio.com",
+});
+
+export default base;
